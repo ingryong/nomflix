f9522cfaf97654aa3995dc120671123bf2fa1a63
ingryong <bbgod2@gmail.com>
2020-07-22 22:21:27 +0900

    settings2

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,7 +1,14 @@
 import React from "react";
+import Router from "Components/Router";
+import Header from "Components/Header";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <>
+      <Header />
+      <Router />
+    </>
+  );
 }
 
 export default App;
