00524ede71d4f37c52cffc71beea7ddb07310a7e
ingryong <bbgod2@gmail.com>
2020-08-10 21:50:58 +0900

    .

--- src/Components/GlobalStyles.js
+++ src/Components/GlobalStyles.js
@@ -12,7 +12,7 @@ const globalStyles = createGlobalStyle`
     }
     body{
         font-family:-apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
-        font-size:16px;
+        font-size:12px;
         background-color:rgba(20, 20, 20, 1);
         color:white;
         padding-top:80px;
